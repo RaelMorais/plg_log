424d82506674af7f5e87baf72d143256bc1ae211
Maycon E. Oliveira <94011800+MayconEspindola@users.noreply.github.com>
2023-09-05 13:57:16 -0300

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,12 +1,12 @@
 <?php
 
-define('DB_NAME_ADM', 'plg_log_adm');
-define('DB_NAME', 'plg_log');
-define('DB_USER_ADM', 'adm_plg_log');
-define('DB_PASSWORD_ADM', 'adm123');
-define('DB_USER', 'user_plg');
+define('DB_NAME_ADM', '');
+define('DB_NAME', '');
+define('DB_USER_ADM', '');
+define('DB_PASSWORD_ADM', '');
+define('DB_USER', '');
 define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_HOST', '');
 
 
 if (!defined('ABSPATH')){
