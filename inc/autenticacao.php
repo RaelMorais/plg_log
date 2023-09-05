41689741cda41a6207504816e4fd1a7cba49edb3
Maycon E. Oliveira <94011800+MayconEspindola@users.noreply.github.com>
2023-09-05 14:19:43 -0300

    correção de caminho

--- inc/autenticacao.php
+++ inc/autenticacao.php
@@ -10,7 +10,7 @@ if (isset($entrar)) {
   $verifica = mysqli_query($conexao,$sql);
   if (mysqli_num_rows($verifica)<=0){
   echo"<script language='javascript' type='text/javascript'>
-    alert('Login e/ou senha incorretos');window.location.href='/src/login.html';</script>";
+    alert('Login e/ou senha incorretos');window.location.href='/src/login.php';</script>";
     die();
   }else{
     setcookie("login",$name);
