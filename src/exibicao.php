c96bae61d157aec71c6a7ed6dd13be4372ec4154
MayconEspindola <mycone10@gmail.com>
2023-09-11 14:23:10 -0300

    consertar a exibição

--- src/exibicao.php
+++ src/exibicao.php
@@ -1,14 +1,45 @@
-<?php include('header.php');?>
+<?php 
+include('header.php');
+include('../inc/conexao.php');
 
-<table class="table table-dark table-striped">
-    <thead>
-        <tr>
+echo "<table class='table table-dark table-striped'>
+        <thead>
+            <tr>
             <th scope='col'>Código</th>
-            <th scope='col'>Produto</th>
-            <th scope='col'>Descrição</th>
             <th scope='col'>Data</th>
-            <th scope='col'>Valor</th>
-            <th scope='col'>Detalhe</th>
-        </tr>
-    </thead>
-<tbody>
+            <th scope='col'>Movimentação</th>
+            <th scope='col'>Pallet 1</th>
+            <th scope='col'>Pallet 2</th>
+            <th scope='col'>Pallet 3</th>
+            <th scope='col'>Pallet 4</th>
+            <th scope='col'>Pallet 5</th>
+            <th scope='col'>Pallet 6</th>
+            </tr>
+        </thead>
+    <tbody>";
+
+$sql = "SELECT pallets.codigo, movimentacao.data, movimentacao.movimentacao, pallets.pallet1, pallets.pallet2, pallets.pallet3, pallets.pallet4, pallets.pallet5, pallets.pallet6
+        FROM pallets
+        JOIN movimentacao ON pallets.codigo = movimentacao.codigo"; 
+
+$result = mysqli_query($conexao, $sql);
+$rows = mysqli_num_rows($result);
+
+if($rows > 0){
+    while($row = mysqli_fetch_assoc($result)){
+        echo "<tr>";
+        echo "<td>" . $row['codigo'] . "</td>";
+        echo "<td>" . $row['data'] . "</td>";
+        echo "<td>" . $row['movimentacao'] . "</td>";
+        echo "<td>" . $row['pallet1'] . "</td>";
+        echo "<td>" . $row['pallet2'] . "</td>";
+        echo "<td>" . $row['pallet3'] . "</td>";
+        echo "<td>" . $row['pallet4'] . "</td>";
+        echo "<td>" . $row['pallet5'] . "</td>";
+        echo "<td>" . $row['pallet6'] . "</td>";
+        echo "</tr>";
+    }
+}
+
+echo "</tbody></table>";
+?>
