67f53417eae2a4b6b6955577afde8f5a1d4f6d60
MayconEspindola <mycone10@gmail.com>
2023-09-21 13:37:12 -0300

    Atualização de Segurança 1.1.1 - md5 salt

--- inc/autenticacao.php
+++ inc/autenticacao.php
@@ -5,20 +5,28 @@ include('conexao_adm.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['nome'];
-    $senha = md5($_POST['senha']);
+    $senha = $_POST['senha'];
     $entrar = $_POST['login'];
 
     if (isset($entrar)) {
-        $sql = "SELECT * FROM usuarios WHERE username ='$name' AND senha = '$senha'";
+        $sql = "SELECT * FROM usuarios WHERE username ='$name'";
         $verifica = mysqli_query($conexao, $sql);
 
         if (mysqli_num_rows($verifica) <= 0) {
             echo "<script>alert('Login e/ou senha incorretos');</script>";
         } else {
-            $_SESSION["username"] = $name; // Armazenar o nome de usuário na sessão
-            //setcookie("username", $name, time() + 3600, "/"); // Armazenar o nome de usuário em um cookie válido por 1 hora
-            header('Location: /src/splash.php');
-            exit();
+            $row = mysqli_fetch_assoc($verifica);
+            $salt = $row['salt'];
+            $senhaComSalt = $senha . $salt;
+            $senhaHash = md5($senhaComSalt);
+
+            if ($senhaHash === $row['senha']) {
+                $_SESSION["username"] = $name; // Armazenar o nome de usuário na sessão
+                header('Location: /src/splash.php');
+                exit();
+            } else {
+                echo "<script>alert('Login e/ou senha incorretos');</script>";
+            }
         }
     }
 }
--- inc/cadastro.php
+++ inc/cadastro.php
@@ -3,7 +3,16 @@ include('conexao_adm.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $nome = $_POST['nome'];
-  $senha = md5($_POST['senha']);
+  $senha = $_POST['senha'];
+
+  // Gerar um salt aleatório
+  $salt = bin2hex(random_bytes(16)); // Use um tamanho adequado para o salt
+
+  // Concatenar o salt com a senha do usuário
+  $senhaComSalt = $senha . $salt;
+
+  // Fazer o hash MD5 da senha com o salt
+  $senhaHash = md5($senhaComSalt);
 
   // Verifica se o nome de usuário já existe
   $sqlVerifica = "SELECT username FROM usuarios WHERE username = '$nome'";
@@ -12,7 +21,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (mysqli_num_rows($selectVerifica) > 0) {
       echo "<script>alert('Nome de usuário já está em uso');window.location.href='/src/cadastro.html'</script>";
   } else {
-      $sql = "INSERT INTO usuarios (senha, username) VALUES ('$senha', '$nome')";
+      // Armazene o salt junto com a senha hash no banco de dados
+      $sql = "INSERT INTO usuarios (senha, salt, username) VALUES ('$senhaHash', '$salt', '$nome')";
       $insert = mysqli_query($conexao, $sql);
 
       if ($insert) {
