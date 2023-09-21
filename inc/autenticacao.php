ecff1c28f3c5ef9f8ff96cc221db1611530dd8e9
MayconEspindola <mycone10@gmail.com>
2023-09-21 13:54:38 -0300

    Atualização de Segurança 1.1.2 - Preparação de SQL

--- inc/autenticacao.php
+++ inc/autenticacao.php
@@ -9,24 +9,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $entrar = $_POST['login'];
 
     if (isset($entrar)) {
-        $sql = "SELECT * FROM usuarios WHERE username ='$name'";
-        $verifica = mysqli_query($conexao, $sql);
+        $sql = "SELECT senha, salt FROM usuarios WHERE username = ?";
+        $stmt = mysqli_prepare($conexao, $sql);
+        mysqli_stmt_bind_param($stmt, "s", $name);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $senhaArmazenada, $salt);
+        mysqli_stmt_fetch($stmt);
 
-        if (mysqli_num_rows($verifica) <= 0) {
-            echo "<script>alert('Login e/ou senha incorretos');</script>";
+        if (password_verify($senha . $salt, $senhaArmazenada)) {
+            $_SESSION["username"] = $name; // Armazenar o nome de usuário na sessão
+            header('Location: /src/splash.php');
+            exit();
         } else {
-            $row = mysqli_fetch_assoc($verifica);
-            $salt = $row['salt'];
-            $senhaComSalt = $senha . $salt;
-            $senhaHash = md5($senhaComSalt);
-
-            if ($senhaHash === $row['senha']) {
-                $_SESSION["username"] = $name; // Armazenar o nome de usuário na sessão
-                header('Location: /src/splash.php');
-                exit();
-            } else {
-                echo "<script>alert('Login e/ou senha incorretos');</script>";
-            }
+            echo "<script>alert('Login e/ou senha incorretos');";
+            header('Location: /src/login.html');
         }
     }
 }
--- inc/cadastro.php
+++ inc/cadastro.php
@@ -1,36 +1,42 @@
 <?php
 include('conexao_adm.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $nome = $_POST['nome'];
-  $senha = $_POST['senha'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
 
-  // Gerar um salt aleatório
-  $salt = bin2hex(random_bytes(16)); // Use um tamanho adequado para o salt
+    // Verifica se o nome de usuário já existe
+    $sqlVerifica = "SELECT username FROM usuarios WHERE username = ?";
+    $stmtVerifica = mysqli_prepare($conexao, $sqlVerifica);
+    mysqli_stmt_bind_param($stmtVerifica, "s", $nome);
+    mysqli_stmt_execute($stmtVerifica);
+    mysqli_stmt_store_result($stmtVerifica);
 
-  // Concatenar o salt com a senha do usuário
-  $senhaComSalt = $senha . $salt;
+    if (mysqli_stmt_num_rows($stmtVerifica) > 0) {
+        echo "<script>alert('Nome de usuário já está em uso');window.location.href='/src/cadastro.html'</script>";
+    } else {
+        // Gerar um salt aleatório
+        $salt = bin2hex(random_bytes(16)); // Use um tamanho adequado para o salt
 
-  // Fazer o hash MD5 da senha com o salt
-  $senhaHash = md5($senhaComSalt);
+        // Concatenar o salt com a senha do usuário
+        $senhaComSalt = $senha . $salt;
 
-  // Verifica se o nome de usuário já existe
-  $sqlVerifica = "SELECT username FROM usuarios WHERE username = '$nome'";
-  $selectVerifica = mysqli_query($conexao, $sqlVerifica);
+        // Fazer o hash da senha com o salt usando o algoritmo bcrypt (mais seguro do que o MD5)
+        $senhaHash = password_hash($senhaComSalt, PASSWORD_BCRYPT);
 
-  if (mysqli_num_rows($selectVerifica) > 0) {
-      echo "<script>alert('Nome de usuário já está em uso');window.location.href='/src/cadastro.html'</script>";
-  } else {
-      // Armazene o salt junto com a senha hash no banco de dados
-      $sql = "INSERT INTO usuarios (senha, salt, username) VALUES ('$senhaHash', '$salt', '$nome')";
-      $insert = mysqli_query($conexao, $sql);
+        // Inserir o usuário no banco de dados
+        $sql = "INSERT INTO usuarios (senha, salt, username) VALUES (?, ?, ?)";
+        $stmt = mysqli_prepare($conexao, $sql);
+        mysqli_stmt_bind_param($stmt, "sss", $senhaHash, $salt, $nome);
+        $insert = mysqli_stmt_execute($stmt);
 
-      if ($insert) {
-          echo "<script>window.location.href='/src/login.html'</script>";
-      } else {
-          echo "<script>alert('Não foi possível cadastrar esse usuário');window.location.href='/src/cadastro.html'</script>";
-      }
-  }
+        if ($insert) {
+            echo "<script>window.location.href='/src/login.html'</script>";
+        } else {
+            echo "<script>alert('Não foi possível cadastrar esse usuário');window.location.href='/src/cadastro.html'</script>";
+        }
+    }
+
     mysqli_close($conexao);
 }
 ?>
