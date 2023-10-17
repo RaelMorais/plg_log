dcb9cccf7fd55ac5c84f2790c2ca5cab5cdc3d82
Maycon Espindola <mycone10@gmail.com>
2023-10-17 09:12:30 -0300

    Conserto nas configurações

---
+++ inc/config.php
@@ -0,0 +1,19 @@
+<?php
+
+define('DB_NAME_ADM', 'plg_log_adm');
+define('DB_NAME', 'plg_log');
+define('DB_USER_ADM', 'adm_plg');
+define('DB_PASSWORD_ADM', 'adm123');
+define('DB_USER', 'adm_plg');
+define('DB_PASSWORD', 'adm123');
+define('DB_HOST', 'mysql');
+
+if (!defined('ABSPATH')){
+    define('ABSPATH', __DIR__);
+}
+
+define('ROOT_PATH', dirname(__FILE__));
+
+if (!defined('DBAPI')){
+    define('DBAPI', ABSPATH . '/conexao.php');
+}
